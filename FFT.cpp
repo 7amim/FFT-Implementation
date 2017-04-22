40acabfb4616f7f47e65e181272b807159fa824f
7amim <tufaruk7@gmail.com>
2017-04-22 03:10:52 -0400

    Finished half the algorithm

--- FFT.cpp
+++ FFT.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
-#include <limits>
+#include <vector>
+#include <complex>
 using namespace std; 
 
-void FFT() {
+vector<complex<double>> FFT(vector<complex<double>> &samples) {
+	int size = samples.size();
 	
+	if (N == 1)
+		return samples;
+	
+	vector<complex<double>> even(size/2, 0);
+	vector<complex<double>> odd(size/2, 0);
+	
+	for (int i = 0; i <= N/2; i++){
+		even[i]=samples[2*i];
+		odd[i]=samples[2*i+1];
+	} 
+	
+	vector<complex<double>> FFT_even(size/2, 0);
+	vector<complex<double>> FFT_odd(size/2, 0);
+	FFT_even = FFT(even);
+	FFT_odd = FFT(odd);	
+	
+	// recompile the the results into frequencybins 
 }
 
 
